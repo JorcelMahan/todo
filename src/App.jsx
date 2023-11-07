f2a75539bb142524e86dbbb9d55990b0e893fd44
Joma Work <johanmfc11@gmail.com>
2023-11-07 15:31:59 -0400

    Update HTML and React code for Todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,132 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const Task = ({ task, removeTask, editTask }) => {
+  const [done, setDone] = useState(task.done);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEdit = () => {
+    setIsEditing(!isEditing);
+  };
 
   return (
-    <>
+    <div>
+      {isEditing ? (
+        <p>editing</p>
+      ) : (
+        <>
+          <input
+            type='checkbox'
+            checked={done}
+            onChange={() => setDone(!done)}
+          />
+          <span
+            style={{
+              textDecoration: done ? 'line-through' : 'none',
+            }}
+          >
+            {task.title}
+          </span>
+        </>
+      )}
+
+      <button onClick={() => removeTask(task.id)}>X</button>
+      <button onClick={toggleEdit}>Edit</button>
+    </div>
+  );
+};
+
+Task.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    done: PropTypes.bool.isRequired,
+  }).isRequired,
+  removeTask: PropTypes.func.isRequired,
+  editTask: PropTypes.func.isRequired,
+};
+
+function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, title: 'Limpiar zapatos', done: false },
+    { id: 2, title: 'Limpiar ventanas', done: false },
+    { id: 3, title: 'Barrer la tienda', done: false },
+  ]);
+
+  const [task, setTask] = useState('');
+
+  const addTask = task => {
+    if (task === '') {
+      alert('Task is empty');
+      return;
+    }
+
+    if (tasks.find(t => t.title === task)) {
+      alert('Already exists');
+      return;
+    }
+
+    const newTask = {
+      id: Date.now(),
+      title: task,
+      done: false,
+    };
+
+    setTasks([...tasks, newTask]);
+    setTask('');
+  };
+
+  const removeTask = id => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
+
+  const editTask = (id, title) => {
+    setTasks(
+      tasks.map(task => {
+        if (task.id === id) {
+          return {
+            ...task,
+            title,
+          };
+        }
+
+        return task;
+      })
+    );
+  };
+
+  return (
+    <div>
+      <h1>Todo</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input
+          type='text'
+          placeholder='New task'
+          onKeyDown={event => {
+            if (event.key === 'Enter') {
+              addTask(event.target.value);
+              event.target.value = '';
+            }
+          }}
+          value={task}
+          onChange={event => {
+            setTask(event.target.value);
+          }}
+        />
+        <button onClick={() => addTask(task)}>+</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      {tasks.map(task => (
+        <Task
+          key={task.id}
+          task={task}
+          removeTask={removeTask}
+          editTask={editTask}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
